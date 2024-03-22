de3f04e8041416425ce9cce92bbab8754f42e6c9
Nathan Lambertson <lambertones@gmail.com>
2024-03-22 10:25:57 -0500

    Add uniqueness to email presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :trips
 
-  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
